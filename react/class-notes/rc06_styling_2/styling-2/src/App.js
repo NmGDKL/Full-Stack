870b84b12468131f0ab956e47c1ac406df780f57
Naim <naimgedikli01@gmail.com>
2022-03-29 13:48:08 +0300

    styling eklendi.

--- react/class-notes/rc06_styling_2/styling-2/src/App.js
+++ react/class-notes/rc06_styling_2/styling-2/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import StyledComponent from "./components/styledComponent/StyledComponent";
+import MaterialUi from "./components/materialUi/MaterialUi"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Styled Component & Material Ui
+      {/* <StyledComponent /> */}
+      <MaterialUi/>
       </header>
+
     </div>
   );
 }
---
+++ react/class-notes/rc06_styling_2/styling-2/src/components/materialUi/button/Button.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import DirectionsIcon from '@mui/icons-material/Directions';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+
+export default function BasicButtons() {
+
+
+
+  return (
+    <>
+   <Button variant="contained" color="secondary" size="large" startIcon={<SaveIcon/>}>
+        KAYDET
+      </Button>      <br /> <br />
+      <Button variant="contained" color='primary' size="large" >SAVE</Button>
+      <br /> <br />
+      <Button variant="outlined" startIcon={<DeleteIcon />} size="large">
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />} size="large">
+        Send
+      </Button>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField error id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+    <br ></br> 
+    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <FormControl variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <TextField
+        id="input-with-icon-textfield"
+        label="TextField"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
+      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="With sx" variant="standard" />
+      </Box>
+    </Box>
+    <br></br>
+    <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <IconButton sx={{ p: '10px' }} aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+        <DirectionsIcon />
+      </IconButton>
+    </Paper>
+    <br></br>
+    <Stack sx={{ width: '100%' }} spacing={2}>
+      <Alert variant="filled" severity="error">
+        This is an error alert — check it out!
+      </Alert>
+      <Alert variant="filled" severity="warning">
+        This is a warning alert — check it out!
+      </Alert>
+      <Alert variant="filled" severity="info">
+        This is an info alert — check it out!
+      </Alert>
+      <Alert variant="filled" severity="success">
+        This is a success alert — check it out!
+      </Alert>
+    </Stack>
+  
+    </>
+  );
+}
