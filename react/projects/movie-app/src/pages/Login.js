63d5d347f7f06a455880ec137e6e183626218999
Naim <naimgedikli01@gmail.com>
2022-04-13 21:42:48 +0300

    v3

--- react/projects/movie-app/src/pages/Login.js
+++ react/projects/movie-app/src/pages/Login.js
@@ -1,6 +1,6 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { googleSignIn, signIn } from "../auth/firebase";
+import { signIn } from "../auth/firebase";
 
 const Login = () => {
   const [email, setEmail] = useState();
@@ -13,55 +13,54 @@ const Login = () => {
     console.log(email, password);
   };
   return (
-  
-  <div className="d-flex justify-content-center">
-    <div className="form-image">
-      <img src={"https://picsum.photos/800/800"} alt="signup" />
-    </div>
-    <div className="register-form">
-      <h1 className="form-title display-3">Login</h1>
-      <form id="register" onSubmit={handleSubmit}>
-        <div className="mb3">
-        <label htmlFor="email" className="form-label">
-          Email :
-        </label>
-        <input 
-        type="text" 
-        id="first-name" 
-        className="form-control" 
-        placeholder="Enter your email adress.." 
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        />
+    <div className="d-flex justify-content-center">
+      {window.innerWidth > 700 && (
+        <div className="form-image">
+          <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
         </div>
-
-        <div className="mb3">
-        <label htmlFor="email" className="form-label">
-          Password :
-        </label>
-        <input 
-        type="text" 
-        id="password" 
-        className="form-control" 
-        placeholder="Enter your password.." 
-        onChange={(e) => setPassword(e.target.value)}
-        required
-        />        
-        </div>
-
-        <input 
-        type="submit" 
-        className="btn btn-primary form-control" 
-        value="Login"
-        // onSubmit={handleSubmit}
-        />
-      </form>
-      <button className="btn btn-primary form-control" onClick={()=>googleSignIn(navigate)}>
-        Continue with Google
-      </button>
+      )}
+      <div className="register-form">
+        <h1 className="form-title display-3">Login</h1>
+        <form id="register" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+            // onSubmit={handleSubmit}
+          />
+        </form>
+        <button className="btn btn-primary form-control">
+          Continue with Google
+        </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default Login;
