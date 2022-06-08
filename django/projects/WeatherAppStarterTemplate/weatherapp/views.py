f4a69c153e4e71b6ff12f2b4990f4e84f190056e
nmgdkl <naimgedikli01@gmail.com>
2022-06-08 21:47:29 +0300

    v1

--- django/projects/WeatherAppStarterTemplate/weatherapp/views.py
+++ django/projects/WeatherAppStarterTemplate/weatherapp/views.py
@@ -3,11 +3,35 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
+
+from weatherapp.models import City
 
 # https://api.openweathermap.org/data/2.5/weather?q=izmir&units=metric&appid=ac083b4c88408ad72956909a36f1093d
 def home(request):
     API_KEY=config('API_KEY')
-    city = 'Sakarya'
+    u_city = request.GET.get('city')
+    if u_city:
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}&units=metric'
+        response = requests.get(url)
+        if response.ok:
+            content = response.json()
+            r_city = content['name']
+            if City.objects.filter(name=r_city):
+                messages.warning(request, f'{r_city} already exists in the database')
+            else:
+                City.objects.create(name=r_city)
+                messages.success(request, f'{r_city} added to the database')
+            
+        else:
+            messages.error(request, 'City not found')
+
+        
+
+
+    content = response.json()
+   
+
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}&units=metric'
     response = requests.get(url)
     content = response.json()
