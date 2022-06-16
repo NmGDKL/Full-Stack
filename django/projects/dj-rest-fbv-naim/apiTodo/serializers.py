d7f43343a4aafdf41e499baa09503784a6492f39
nmgdkl <naimgedikli01@gmail.com>
2022-06-16 21:22:50 +0300

    v3

--- django/projects/dj-rest-fbv-naim/apiTodo/serializers.py
+++ django/projects/dj-rest-fbv-naim/apiTodo/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    
+    todo_detail = serializers.HyperlinkedIdentityField(view_name="todo-detail")
     class Meta:
         model= Todo
-        fields = "__all__"
-        
+        fields = ("id", "task","description","priority", "done", "todo_detail" ,  "updateDate", "createdDate" )
         
